62a0417cf82970d1220d29fe677d98baf02662b5
Juancorrea333 <juan.correa.zapata@correounivalle.edu.co>
2026-04-03 13:20:58 -0500

    Jugadores listos y cartas repartidas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.*;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final Random rand = new Random();
+
+    public static void main(String[] args) {
+        System.out.println("=== YU-GI-OH! DUELO ===");
+        System.out.print("Nombre del duelista 1: ");
+        Jugador j1 = new Jugador(sc.nextLine());
+        System.out.print("Nombre del duelista 2: ");
+        Jugador j2 = new Jugador(sc.nextLine());
+
+    
+        List<Carta> mazoCompleto = crearCartas();
+        Collections.shuffle(mazoCompleto);
+        for (int i = 0; i < 20; i++) j1.agregarAlMazo(mazoCompleto.get(i));
+        for (int i = 20; i < 40; i++) j2.agregarAlMazo(mazoCompleto.get(i));
+
+
+        for (int i = 0; i < 5; i++) {
+            j1.robarCarta();
+            j2.robarCarta();
+        }
     }
 }
